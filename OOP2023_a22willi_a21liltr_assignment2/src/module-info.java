a214d05e0f67e2e110a859013a80af184af70b1b
William Lindholm <william_lindholm@outlook.com>
2023-01-25 14:12:08 +0100

    Added todo module and module-info.java file

---
+++ OOP2023_a22willi_a21liltr_assignment2/src/module-info.java
@@ -0,0 +1,13 @@
+/**
+ * 
+ */
+/**
+ * @author Willi
+ *
+ */
+module OOP2023_a22willi_a21liltr_assignment2 {
+	requires java.base;
+    requires java.se;
+    requires it401g.todo;
+    exports assignment2;
+}
